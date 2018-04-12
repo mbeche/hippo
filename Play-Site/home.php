8653a6b3e6a5b1bc1d0e157b64d52aecc78e5ce3
Mike <mb.johnsonbece@gmail.com>
2018-04-11 21:09:44 -0500

    first commit

---
+++ Play-Site/home.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link href="https://fonts.googleapis.com/css?family=Just+Me+Again+Down+Here|Lato:400,400i,700|Montserrat:400,800" rel="stylesheet">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.9/css/all.css" integrity="sha384-5SOiIsAziJl6AWe0HWRKTXlfcSHKmYV4RBF18PPJ173Kzn7jzMyFuTtk8JA7QQG1" crossorigin="anonymous">
+        <link rel="stylesheet" href="style.css">
+        <title>Document</title>
+    </head>
+    <body>
+        <div class="container">
+            <div class="title"><img src="title.png" alt="a screenshot of the title - will replace with text"></div>
+            <div class="main element">
+                <div class="sub">
+                    <p>Where's my box at?</p>
+                    <div class="box">A box?</div>
+                </div>
+                <div class="tags">
+                    <p>Categories: Christmas, ISU, Social Life, Videos</p>
+                    <p>Tags: Admissions, school life, Cyclones, Iowa State University, ISU, memorial union, Midnight pancakes, traditions</p>
+                </div>
+            </div>
+            <div class="bottom foldtl">
+                <div class="page ">
+                    <h2>Headline</h2>
+                    <p>Lorem ipsum dolor sit amet...</p>
+                </div>
+            </div>
+            <div class="sidebar"> 
+                <div class="tabs">
+                    <div class="search"><i class="fas fa-search"></i></div>
+                    <div class="cast"><i class="far fa-smile"></i><br><div class="tab">Cast & Crew</div></div>
+                    <div class="vid"><i class="far fa-smile"></i><br><div class="tab">Videos</div></div>
+                    <div class="topics"><i class="far fa-smile"></i><br><div class="tab">Topics</div></div>
+                    <div class="archive"><i class="far fa-smile"></i><br><div class="tab">Archive</div></div>
+                </div>
+                <div class="menu foldtl">
+
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
