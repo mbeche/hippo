23e6bedfdfff08090abd006b8f1de80ec309500c
Mike <mb.johnsonbece@gmail.com>
2018-04-14 17:22:17 -0500

    Major updates styling

--- Play-Site/home.php
+++ Play-Site/home.php
@@ -5,24 +5,31 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <link href="https://fonts.googleapis.com/css?family=Just+Me+Again+Down+Here|Lato:400,400i,700|Montserrat:400,800" rel="stylesheet">
-        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.9/css/all.css" integrity="sha384-5SOiIsAziJl6AWe0HWRKTXlfcSHKmYV4RBF18PPJ173Kzn7jzMyFuTtk8JA7QQG1" crossorigin="anonymous">
         <link rel="stylesheet" href="style.css">
-        <title>Document</title>
+        <title>Mellanie's Blog</title>
     </head>
     <body>
         <div class="container">
-            <div class="title"><img src="title.png" alt="a screenshot of the title - will replace with text"></div>
+            <div class="title">
+				<div id="com">.com</div>
+				<span id="school-life">school life</span>
+			</div>
             <div class="main element">
                 <div class="sub">
-                    <p>Where's my box at?</p>
-                    <div class="box">A box?</div>
+					<img class="headshot" src="img/mellanie-post.png" />
+					<div class="author">by Mellanie</div>
+                    <h2>Student in New York: Statue of Liberty</h2>
+					<div class="date">December 13, 2011</div>
+					<p>Easily the best part about being in New York for a football game is the location! There is so much to do in New York, I definitely didn’t do everything I wanted to do!</p>
+
+					<p>I also went to the top of 30 Rock!  This is the view from the top facing the Empire State  Building!</p>
                 </div>
                 <div class="tags">
                     <p>Categories: Christmas, ISU, Social Life, Videos</p>
                     <p>Tags: Admissions, school life, Cyclones, Iowa State University, ISU, memorial union, Midnight pancakes, traditions</p>
                 </div>
             </div>
-            <div class="bottom foldtl">
+            <div class="bottom">
                 <div class="page ">
                     <h2>Headline</h2>
                     <p>Lorem ipsum dolor sit amet...</p>
@@ -30,25 +37,89 @@
             </div>
             <div class="sidebar"> 
                 <div class="tabs">
-                    <div class="search"><i class="fas fa-search"></i></div>
-                    <div class="cast"><div class="tab">Cast & Crew<i class="far fa-smile"></i></div></div>
-                    <div class="vid"><div class="tab">Videos<i class="far fa-smile"></i></div></div>
-                    <div class="topics"><div class="tab">Topics<i class="far fa-smile"></i></div></div>
-                    <div class="archive"><div class="tab">Archive<i class="far fa-smile"></i></div></div>
+                    <div class="search">
+						<div class="search-shadow"></div>
+						<span class="fas">M</span>
+						<img src="img/search.png" class="search-text"/>
+					</div>
+                    <div class="cast"><div class="tab">Cast & Crew<div class="far">y</div></div></div>
+                    <div class="vid"><div class="tab">Videos<div class="far">V</div></div></div>
+                    <div class="topics"><div class="tab">Topics<div class="far">g</div></div></div>
+                    <div class="archive"><div class="tab">Archive<div class="far">5</div></div></div>
                 </div>
-                <div class="menu foltl">
-                    <div class="heading">Lastest blogs from Mellanie</div>
-                    <div>Post 1</div>
-                    <div>Post 2</div>
-                    <div>Post 3</div>
-                    <div>Post 4</div>
-                    <div>Post 5</div>
-                    <div class="heading">Latest posts</div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
+
+                <div class="menu">
+
+					<div class="heading">Lastest posts from <span id="auth">Mellanie</span></div>
+                    <div class="post">The fire is so delightful (movie)
+						<br><span class="date">posted on December 15, 2011</span>					
+					</div>
+                    <hr>
+					<div class="post">Agronomy Courtyard
+						<br><span class="date">posted on December 15, 2011</span>
+					</div>
+                    <hr>
+					<div class="post">College of Technology and Sciences Student Council
+						<br><span class="date">posted on December 15, 2011</span>
+					</div>
+					<hr>
+					<div class="post">Mellanie’s Intro (movie)
+						<br><span class="date">posted on December 15, 2011</span>
+					</div>
+					<hr>
+					<div class="post">Greetings from just up the road
+						<br><span class="date">posted on December 15, 2011</span>
+					</div>
+					<hr>
+					<div class="post">
+						<div class="button author-more">View All</div>
+					</div>
+
+					<div class="recent-posts">
+						<div class="heading">Latest posts</div>
+						<div class="post">The fire is so delightful (movie)
+							<br><span class="date">posted on December 15, 2011</span>					
+						</div>
+						<hr>
+						<div class="post">Agronomy Courtyard
+							<br><span class="date">posted on December 15, 2011</span>
+						</div>
+						<hr>
+						<div class="post">College of Technology and Sciences Student Council
+							<br><span class="date">posted on December 15, 2011</span>
+						</div>
+						<hr>
+						<div class="post">Mellanie’s Intro (movie)
+							<br><span class="post-author"><em>by</em> Author Name </span><span class="date">posted on December 15, 2011</span>
+						</div>
+						<hr>
+						<div class="post">Greetings from just up the road
+							<br><span class="date">posted on December 15, 2011</span>
+						</div>
+						<hr>
+						<div class="post">
+							<div class="button all-more">View All</div>
+						</div>
+					</div>
+					<hr class="menu-hr"/>
+
+					<div class="btm-menu">
+						<a href="#">What is School Life?</a>
+						<br/>
+						<a href="#">Ask a Question</a>
+						<br/>
+						<a href="#">Live Broadcast Schedule</a>
+						<br/>
+						<a href="#">Join the Cast</a>
+						<br/>
+						<a href="#">Subscribe</a>
+					</div>
+					<hr class="menu-hr"/>
+
+					<div class="recent-posts">
+						<div class="copyright">&copy; 2012 School Life.</div>
+					</div>
+
                 </div>
             </div>
         </div>
