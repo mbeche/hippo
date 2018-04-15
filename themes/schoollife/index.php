cdbdf159f8d7b2fb892f7a16baa70f94056d6bb3
Mike <mb.johnsonbece@gmail.com>
2018-04-14 21:54:21 -0500

    added a themes folder to upload to wordpress

---
+++ themes/schoollife/index.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link href="https://fonts.googleapis.com/css?family=Just+Me+Again+Down+Here|Lato:400,400i,700|Montserrat:400,800" rel="stylesheet">
+        <link href="my-style.css" rel="stylesheet">
+        <title>Mellanie's Blog</title>
+    </head>
+    <body>
+			<div class="menu">
+				<div class="tabs">
+					<div class="search">
+						<div class="search-shadow"></div>
+						<span class="fas">M</span>
+						<img src="img/search.png" class="search-text"/>
+					</div>
+					<div class="cast"><div class="tab">Cast & Crew</div><div class="far">y</div></div>
+					<div class="vid"><div class="tab">Videos</div><div class="far">V</div></div>
+					<div class="topics"><div class="tab">Topics</div><div class="far">g</div></div>
+					<div class="archive"><div class="tab">Archive</div><div class="far">5</div></div>
+				</div>
+
+				<div class="sidebar"> 
+					
+					<div class="blogger-posts">
+						<div class="heading">Lastest posts from <span id="auth">Mellanie</span></div>
+						<div class="post">The fire is so delightful (movie)
+							<br><span class="date">posted on December 15, 2011</span>					
+						</div>
+						<hr>
+						<div class="post">Agronomy Courtyard
+							<br><span class="date">posted on December 15, 2011</span>
+						</div>
+						<hr>
+						<div class="post">College of Technology and Sciences Student Council
+							<br><span class="date">posted on December 15, 2011</span>
+						</div>
+						<hr>
+						<div class="post">Mellanie’s Intro (movie)
+							<br><span class="date">posted on December 15, 2011</span>
+						</div>
+						<hr>
+						<div class="post">Greetings from just up the road
+							<br><span class="date">posted on December 15, 2011</span>
+						</div>
+						<hr>
+						<div class="post">
+							<div class="button author-more">View All</div>
+						</div>
+					</div>
+					
+					<div class="recent-posts">
+						<div class="heading">Latest posts</div>
+						<div class="post">The fire is so delightful (movie)
+							<br><span class="date">posted on December 15, 2011</span>					
+						</div>
+						<hr>
+						<div class="post">Agronomy Courtyard
+							<br><span class="date">posted on December 15, 2011</span>
+						</div>
+						<hr>
+						<div class="post">College of Technology and Sciences Student Council
+							<br><span class="date">posted on December 15, 2011</span>
+						</div>
+						<hr>
+						<div class="post">Mellanie’s Intro (movie)
+							<br><span class="post-author"><em>by</em> Author Name </span><span class="date">posted on December 15, 2011</span>
+						</div>
+						<hr>
+						<div class="post">Greetings from just up the road
+							<br><span class="date">posted on December 15, 2011</span>
+						</div>
+						<hr>
+						<div class="post">
+							<div class="button all-more">View All</div>
+						</div>
+					</div>
+					<hr class="menu-hr"/>
+
+					<div class="btm-menu">
+						<a href="#">What is School Life?</a>
+						<br/>
+						<a href="#">Ask a Question</a>
+						<br/>
+						<a href="#">Live Broadcast Schedule</a>
+						<br/>
+						<a href="#">Join the Cast</a>
+						<br/>
+						<a href="#">Subscribe</a>
+					</div>
+					
+					<hr class="menu-hr"/>
+
+					<div class="recent-posts">
+						<div class="copyright">&copy; 2012 School Life.</div>
+					</div>
+
+				</div>
+			</div>
+
+			<div class="title">
+				<div id="com">.com</div>
+				<span id="school-life">school life</span>
+			</div>
+			
+            <div class="main">
+                <div class="sub">
+					<img class="headshot" src="img/mellanie-post.png" />
+					<div class="author">by Mellanie</div>
+                    <h2>Student in New York: Statue of Liberty</h2>
+					<div class="date">December 13, 2011</div>
+					<p>Easily the best part about being in New York for a football game is the location! There is so much to do in New York, I definitely didn’t do everything I wanted to do!</p>
+
+					<p>I also went to the top of 30 Rock!  This is the view from the top facing the Empire State  Building!</p>
+                </div>
+				
+                <div class="tags">
+                    <p>Categories: Christmas, ISU, Social Life, Videos</p>
+                    <p>Tags: Admissions, school life, Cyclones, Iowa State University, ISU, memorial union, Midnight pancakes, traditions</p>
+                </div>
+				
+            </div>
+			
+            <div class="bottom">
+            
+				<div class="page ">
+                    <h2>Headline</h2>
+                    <p>Lorem ipsum dolor sit amet...</p>
+					</div>
+            </div>
+			
+        </div>
+    </body>
+</html>
