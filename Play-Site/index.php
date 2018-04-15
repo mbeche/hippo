f0fbe869efaa9951689fbf282e8bd99165f5b852
Mike <mb.johnsonbece@gmail.com>
2018-04-15 12:59:30 -0500

    Layout complete and compatable with different size screens

--- Play-Site/index.php
+++ Play-Site/index.php
@@ -9,103 +9,13 @@
         <title>Mellanie's Blog</title>
     </head>
     <body>
-			<div class="menu" id="full">
-				<div class="tabs">
-					<div class="search">
-						<div class="search-shadow"></div>
-						<span class="fas">M</span>
-						<img src="img/search.png" class="search-text"/>
-					</div>
-					<div class="cast"><div class="tab">Cast & Crew</div><div class="far">y</div></div>
-					<div class="vid"><div class="tab">Videos</div><div class="far">V</div></div>
-					<div class="topics"><div class="tab">Topics</div><div class="far">g</div></div>
-					<div class="archive"><div class="tab">Archive</div><div class="far">5</div></div>
-				</div>
-
-				<div class="sidebar">
-
-					<div class="blogger-posts">
-						<div class="heading">Lastest posts from <span id="auth">Mellanie</span></div>
-						<div class="post">The fire is so delightful (movie)
-							<br><span class="date">posted on December 15, 2011</span>
-						</div>
-						<hr>
-						<div class="post">Agronomy Courtyard
-							<br><span class="date">posted on December 15, 2011</span>
-						</div>
-						<hr>
-						<div class="post">College of Technology and Sciences Student Council
-							<br><span class="date">posted on December 15, 2011</span>
-						</div>
-						<hr>
-						<div class="post">Mellanie’s Intro (movie)
-							<br><span class="date">posted on December 15, 2011</span>
-						</div>
-						<hr>
-						<div class="post">Greetings from just up the road
-							<br><span class="date">posted on December 15, 2011</span>
-						</div>
-						<hr>
-						<div class="post">
-							<div class="button author-more">View All</div>
-						</div>
-					</div>
-
-					<div class="recent-posts">
-						<div class="heading">Latest posts</div>
-						<div class="post">The fire is so delightful (movie)
-							<br><span class="date">posted on December 15, 2011</span>
-						</div>
-						<hr>
-						<div class="post">Agronomy Courtyard
-							<br><span class="date">posted on December 15, 2011</span>
-						</div>
-						<hr>
-						<div class="post">College of Technology and Sciences Student Council
-							<br><span class="date">posted on December 15, 2011</span>
-						</div>
-						<hr>
-						<div class="post">Mellanie’s Intro (movie)
-							<br><span class="post-author"><em>by</em> Author Name </span><span class="date">posted on December 15, 2011</span>
-						</div>
-						<hr>
-						<div class="post">Greetings from just up the road
-							<br><span class="date">posted on December 15, 2011</span>
-						</div>
-						<hr>
-						<div class="post">
-							<div class="button all-more">View All</div>
-						</div>
-					</div>
-					<hr class="menu-hr"/>
-
-					<div class="btm-menu">
-						<a href="#">What is School Life?</a>
-						<br/>
-						<a href="#">Ask a Question</a>
-						<br/>
-						<a href="#">Live Broadcast Schedule</a>
-						<br/>
-						<a href="#">Join the Cast</a>
-						<br/>
-						<a href="#">Subscribe</a>
-					</div>
-
-					<hr class="menu-hr"/>
-
-					<div class="recent-posts">
-						<div class="copyright">&copy; 2012 School Life.</div>
-					</div>
-
-				</div>
-			</div>
-
-			<div class="title">
-				<div id="com">.com</div>
-				<span id="school-life">school life</span>
-			</div>
 
       <div class="content">
+        <div class="title">
+  				<div id="com">.com</div>
+  				<span id="school-life">school life</span>
+  			</div>
+
         <div class="main">
           <div class="sub">
             <img class="headshot" src="img/mellanie-post.png" />
@@ -127,6 +37,97 @@
                   <h2>Headline</h2>
                   <p>Lorem ipsum dolor sit amet...</p>
 				  </div>
+        </div>
+      </div>
+
+      <div class="menu" id="full">
+        <div class="tabs">
+          <div class="search">
+            <div class="search-shadow"></div>
+            <span class="fas">M</span>
+            <img src="img/search.png" class="search-text"/>
+          </div>
+          <div class="cast"><div class="tab">Cast & Crew</div><div class="far">y</div></div>
+          <div class="vid"><div class="tab">Videos</div><div class="far">V</div></div>
+          <div class="topics"><div class="tab">Topics</div><div class="far">g</div></div>
+          <div class="archive"><div class="tab">Archive</div><div class="far">5</div></div>
+        </div>
+
+        <div class="sidebar">
+
+          <div class="blogger-posts">
+            <div class="heading">Lastest posts from <span id="auth">Mellanie</span></div>
+            <div class="post">The fire is so delightful (movie)
+              <br><span class="date">posted on December 15, 2011</span>
+            </div>
+            <hr>
+            <div class="post">Agronomy Courtyard
+              <br><span class="date">posted on December 15, 2011</span>
+            </div>
+            <hr>
+            <div class="post">College of Technology and Sciences Student Council
+              <br><span class="date">posted on December 15, 2011</span>
+            </div>
+            <hr>
+            <div class="post">Mellanie’s Intro (movie)
+              <br><span class="date">posted on December 15, 2011</span>
+            </div>
+            <hr>
+            <div class="post">Greetings from just up the road
+              <br><span class="date">posted on December 15, 2011</span>
+            </div>
+            <hr>
+            <div class="post">
+              <div class="button author-more">View All</div>
+            </div>
+          </div>
+
+          <div class="recent-posts">
+            <div class="heading">Latest posts</div>
+            <div class="post">The fire is so delightful (movie)
+              <br><span class="date">posted on December 15, 2011</span>
+            </div>
+            <hr>
+            <div class="post">Agronomy Courtyard
+              <br><span class="date">posted on December 15, 2011</span>
+            </div>
+            <hr>
+            <div class="post">College of Technology and Sciences Student Council
+              <br><span class="date">posted on December 15, 2011</span>
+            </div>
+            <hr>
+            <div class="post">Mellanie’s Intro (movie)
+              <br><span class="post-author"><em>by</em> Author Name </span><span class="date">posted on December 15, 2011</span>
+            </div>
+            <hr>
+            <div class="post">Greetings from just up the road
+              <br><span class="date">posted on December 15, 2011</span>
+            </div>
+            <hr>
+            <div class="post">
+              <div class="button all-more">View All</div>
+            </div>
+          </div>
+          <hr class="menu-hr"/>
+
+          <div class="btm-menu">
+            <a href="#">What is School Life?</a>
+            <br/>
+            <a href="#">Ask a Question</a>
+            <br/>
+            <a href="#">Live Broadcast Schedule</a>
+            <br/>
+            <a href="#">Join the Cast</a>
+            <br/>
+            <a href="#">Subscribe</a>
+          </div>
+
+          <hr class="menu-hr"/>
+
+          <div class="recent-posts">
+            <div class="copyright">&copy; 2012 School Life.</div>
+          </div>
+
         </div>
       </div>
     </body>
