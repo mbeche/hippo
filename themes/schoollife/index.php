27b11bca85b1aa98824d72dcfd28e06e42c78588
Mike <mb.johnsonbece@gmail.com>
2018-04-19 20:12:20 -0500

    Make footer and header.

--- themes/schoollife/index.php
+++ themes/schoollife/index.php
@@ -1,23 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <link href="https://fonts.googleapis.com/css?family=Just+Me+Again+Down+Here|Lato:400,400i,700|Montserrat:400,800" rel="stylesheet">
-        <link rel="stylesheet" href='<?php echo get_bloginfo('template_directory'); ?>/my-style.css'>
-        <title>Mellanie's Blog</title>
-    </head>
-    <body>
-      <div class="disclaimer">
-        This site/theme is a demo. <br>
-        <a href='<?php echo get_bloginfo('template_directory'); ?>/img/SchoolLife_Single.png'>Click here</a> to view PSD capture I am emulating.
-      </div>
-      <div class="content">
-        <div class="title">
-  				<a href="#"><div id="com">.com</div>
-  				<span id="school-life">school life</span></a>
-  			</div>
+    <?php get_header(); ?>
 
         <div class="main">
           <div class="sub">
@@ -138,5 +119,3 @@
 
         </div>
       </div>
-    </body>
-</html>
